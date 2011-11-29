3ce55ff20764d719f8280024f5043ccd80a632d0
Jude Mwenda <judemwenda@gmail.com>
2011-11-29 15:50:22 -0500

    setup file for the registry application

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='geonode-registry',
+      version='0.1',
+      description='Functions to help identify broken layers and maps',
+      author='Jude Mwenda',
+      author_email='judemwenda@gmail.com',
+      url='http://www.github.com/jmwenda/geonode-registry',
+      packages=['registry', ],
+     )
