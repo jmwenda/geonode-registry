e90c7488e34f2614af4a2e3047828c7235f67321
Jude Mwenda <judemwenda@gmail.com>
2011-12-01 14:23:21 -0500

    added the admin geonodestatus

--- registry/admin.py
+++ registry/admin.py
@@ -1,5 +1,5 @@
 from geonode.maps.models import Map, Layer, MapLayer, Contact, ContactRole, Role
-from registry.models import GeoNodeInstance
+from registry.models import GeoNodeInstance,GeoNodeStatus
 from django.contrib.contenttypes.models import ContentType
 from django.contrib import admin
 from django.http import HttpResponseRedirect
@@ -8,3 +8,4 @@ from django.contrib.contenttypes import generic
 
 
 admin.site.register(GeoNodeInstance)
+admin.site.register(GeoNodeStatus)
